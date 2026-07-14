8313055c44fcb42939fbedb35bcbf04bf6388cbd
Krish <sujal99903@gmail.com>
2026-07-14 23:28:04 +0530

    Show DB error message

--- server/config/db.js
+++ server/config/db.js
@@ -1,12 +1,12 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
-const connectDb=async ()=>{
-    try {
-        await mongoose.connect(process.env.MONGODB_URL)
-        console.log("db connected")
-    } catch (error) {
-        console.log("db error")
-    }
-}
+const connectDb = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URL);
+    console.log("db connected");
+  } catch (error) {
+    console.log("DB ERROR:", error.message);
+  }
+};
 
-export default connectDb
+export default connectDb;
