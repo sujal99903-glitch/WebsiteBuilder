d5ab9c863c3af91fa5ec048b2b2f40b36a8befd9
Krish <sujal99903@gmail.com>
2026-07-14 23:40:21 +0530

    Fix production cookies

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -16,8 +16,8 @@ try {
 
     res.cookie("token",token,{
         httpOnly:true,
-        secure:false,
-        sameSite:"strict",
+        secure:true,
+        sameSite:"none",
         maxAge:7*24*60*60*1000
     })
 
@@ -27,18 +27,16 @@ try {
     return res.status(500).json({message:`google auth error ${error}`})
 }
 }
+export const logOut = async (req, res) => {
+  try {
+    res.clearCookie("token", {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+    });
 
-
-export const logOut=async (req,res)=>{
-try {
-     res.clearCookie("token",{
-        httpOnly:true,
-        secure:false,
-        sameSite:"strict"
-    })
-
-    return res.status(200).json({message :"log out successfully"})
-} catch (error) {
-    return res.status(500).json({message:`log out error ${error}`})
-}
-}
+    return res.status(200).json({ message: "log out successfully" });
+  } catch (error) {
+    return res.status(500).json({ message: `log out error ${error}` });
+  }
+};
